7ecec586e36958a033bc607e51a167689e15552f
David Ouagne <david.ouagne@phast.fr>
2021-06-04 14:31:04 +0200

    drug formulary basis

--- src/app/common/fhir/fhir.resource.type.ts
+++ src/app/common/fhir/fhir.resource.type.ts
@@ -1,6 +1,7 @@
 export enum ResourceType {
     Attachment = 'Attachment',
     Bundle = 'Bundle',
+    Composition = 'Composition',
     Patient = 'Patient',
     Practitioner = 'Practitioner',
     Library = 'Library',
--- src/app/common/fhir/fhir.type.guard.ts
+++ src/app/common/fhir/fhir.type.guard.ts
@@ -1,37 +1,45 @@
 import { fhir } from './fhir.types';
 import { ResourceType } from './fhir.resource.type';
+import Composition = fhir.Composition;
+import Patient = fhir.Patient;
+import Practitioner = fhir.Practitioner;
 
 export class FhirTypeGuard {
 
-    static isPatient(x: unknown): x is fhir.Patient {
-        return (x as fhir.Patient).resourceType !== undefined
-            &&  (x as fhir.Patient).resourceType === ResourceType.Patient;
-    }
+  static isComposition(x: unknown): x is Composition {
+    return (x as Composition).resourceType !== undefined
+      &&  (x as Composition).resourceType === ResourceType.Composition;
+  }
 
-    static isPractitioner(x: unknown): x is fhir.Practitioner {
-      return (x as fhir.Practitioner).resourceType !== undefined
-        &&  (x as fhir.Practitioner).resourceType === ResourceType.Practitioner;
-    }
+  static isPatient(x: unknown): x is Patient {
+      return (x as Patient).resourceType !== undefined
+          &&  (x as Patient).resourceType === ResourceType.Patient;
+  }
 
-    static isLibrary(x: unknown): x is fhir.Library {
-        return (x as fhir.Library).resourceType !== undefined
-            &&  (x as fhir.Library).resourceType === ResourceType.Library;
-    }
+  static isPractitioner(x: unknown): x is Practitioner {
+    return (x as Practitioner).resourceType !== undefined
+      &&  (x as Practitioner).resourceType === ResourceType.Practitioner;
+  }
 
-    static isBundle(x: unknown): x is fhir.Bundle {
-        return (x as fhir.Bundle).resourceType !== undefined
-            &&  (x as fhir.Bundle).resourceType === ResourceType.Bundle;
-    }
+  static isLibrary(x: unknown): x is fhir.Library {
+      return (x as fhir.Library).resourceType !== undefined
+          &&  (x as fhir.Library).resourceType === ResourceType.Library;
+  }
 
-    static isParameters(x: unknown): x is fhir.Parameters {
-        return (x as fhir.Parameters).resourceType !== undefined
-            &&  (x as fhir.Parameters).resourceType === ResourceType.Parameters;
-    }
+  static isBundle(x: unknown): x is fhir.Bundle {
+      return (x as fhir.Bundle).resourceType !== undefined
+          &&  (x as fhir.Bundle).resourceType === ResourceType.Bundle;
+  }
 
-    static isMedicationRequest(x: unknown): x is fhir.MedicationRequest {
-      return (x as fhir.MedicationRequest).resourceType !== undefined
-        &&  (x as fhir.MedicationRequest).resourceType === ResourceType.MedicationRequest;
-    }
+  static isParameters(x: unknown): x is fhir.Parameters {
+      return (x as fhir.Parameters).resourceType !== undefined
+          &&  (x as fhir.Parameters).resourceType === ResourceType.Parameters;
+  }
+
+  static isMedicationRequest(x: unknown): x is fhir.MedicationRequest {
+    return (x as fhir.MedicationRequest).resourceType !== undefined
+      &&  (x as fhir.MedicationRequest).resourceType === ResourceType.MedicationRequest;
+  }
 
   static isMedicationKnowledge(x: unknown): x is fhir.MedicationKnowledge {
     return (x as fhir.MedicationKnowledge).resourceType !== undefined
