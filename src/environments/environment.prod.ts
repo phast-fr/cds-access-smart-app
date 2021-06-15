910653c5cfdebadab13e221dfd37dc1ff7377bb1
David Ouagne <david.ouagne@phast.fr>
2021-06-15 08:19:38 +0200

    Update configuration for prod mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  client_id: {
+    prescription: '6965909a-8366-4236-ac7f-d754cd40bbdd',
+    formulary: 'c9344bf2-96e2-4274-9a33-4ba502b25a42'
+  },
+  scope: {
+    prescription: 'online_access profile openid fhirUser launch launch/user user/*.* patient/*.*',
+    formulary: 'online_access profile openid fhirUser launch user/*.*'
+  },
+  display_language: 'fr-FR',
+  cds_hooks_url: 'https://cds-access.phast.fr:8443/cqf-ruler-r4',
+  cio_dc_url: 'https://jade.phast.fr/resources-server/api/FHIR',
+  cio_dc_credential: 'ZGF2aWQub3VhZ25lQHBoYXN0LmZyOjIncU1oa21+WFojdnpW',
+  tio_url: 'https://jade.phast.fr/resources-server/api/FHIR',
+  tio_credential: 'ZGF2aWQub3VhZ25lQHBoYXN0LmZyOjIncU1oa21+WFojdnpW',
+  drug_formulary_resource_type: 'MedicationKnowledge'
 };
