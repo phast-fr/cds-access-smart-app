d73ec6385003a52023e64ac72ded1e41ce3a394d
David Ouagne <david.ouagne@phast.fr>
2021-09-17 09:58:59 +0200

    fix(history): history restore

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/prescribedc'),
+      dir: require('path').join(__dirname, './coverage/cds-access'),
       subdir: '.',
       reporters: [
         { type: 'html' },
