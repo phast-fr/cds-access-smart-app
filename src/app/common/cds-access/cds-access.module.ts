4e55bb82ebdb2d1027ee8fd580eb7d2bbbd22b78
David Ouagne <david.ouagne@phast.fr>
2021-10-08 15:33:41 +0200

    feat(cql-editor): add cql-editor ref to cds-access.module.ts

--- src/app/common/cds-access/cds-access.module.ts
+++ src/app/common/cds-access/cds-access.module.ts
@@ -16,6 +16,7 @@ import {FhirModule} from '../fhir/fhir.module';
 
 import {StateService} from './services/state.service';
 import {PhastCioDcService} from './services/phast.cio.dc.service';
+import {PhastCioCdsService} from './services/phast.cio.cds.service';
 import {PhastTioService} from './services/phast.tio.service';
 
 import {HeaderComponent} from './components/header/header.component';
@@ -23,6 +24,7 @@ import {FooterComponent} from './components/footer/footer.component';
 
 import {AgePipe} from './pipes/age.pipe';
 import {MyI18nSelectPipe} from './pipes/i18n.pipe';
+import {PhastCQLService} from './services/phast.cql.service';
 
 /**
  * @description
@@ -31,28 +33,30 @@ import {MyI18nSelectPipe} from './pipes/i18n.pipe';
  */
 @NgModule({
   declarations: [
-    HeaderComponent,
-    FooterComponent,
-    AgePipe,
-    MyI18nSelectPipe
+      HeaderComponent,
+      FooterComponent,
+      AgePipe,
+      MyI18nSelectPipe
   ],
   imports: [
-    CommonModule,
-    FlexLayoutModule,
-    MatIconModule,
-    MatCardModule,
-    MatTooltipModule,
-    FhirModule
+      CommonModule,
+      FlexLayoutModule,
+      MatIconModule,
+      MatCardModule,
+      MatTooltipModule,
+      FhirModule
   ],
   exports: [
-    HeaderComponent,
-    FooterComponent,
-    AgePipe
+      HeaderComponent,
+      FooterComponent,
+      AgePipe
   ],
   providers: [
-    StateService,
-    PhastCioDcService,
-    PhastTioService
+      StateService,
+      PhastCioDcService,
+      PhastTioService,
+      PhastCioCdsService,
+      PhastCQLService
   ]
 })
 export class CdsAccessModule { }
