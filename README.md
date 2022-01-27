ee157547d89ed3370f837fe5da738d36f6160922
David Ouagne <david.ouagne@phast.fr>
2022-01-27 18:33:03 +0100

    ci: update env configuration

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # CDS-ACCESS Smart on FHIR App
 
+[![MIT License][license-image]][license]![build](https://github.com/phast-fr/cds-access-smart-app/actions/workflows/ci.yml/badge.svg)
+
 Smart on FHIR App is a part of CDS ACCESS project which aims to facilitate the integration of CDS in the hospital.  
 
 ## Install
 
-1. Have installed the prerequisites (git, Node.js v14.x.x)
+1. Have installed the prerequisites (git, node.js v16.x)
 2. Clone the project: git clone https://github.com/phast-fr/cds-access-smart-app.git (for more details: https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository)
 3. Install the application dependencies: in the cds-access-smart-app directory, run npm install
 4. Creation of an account on the Logica Sandbox: https://sandbox.logicahealth.org/ (or another Smart on FHIR platform)
@@ -15,3 +17,6 @@ Smart on FHIR App is a part of CDS ACCESS project which aims to facilitate the i
    7.2. in the newly created credential.ts file, update the client_id of the prescription module with the UUID obtained during the registration of the App and the identifiers obtained from PHAST in cio_dc_credential and tio_credential
 8. Start listening to the server: in the cds-access-smart-app directory, run npm start
 9. Launch the application from the Logica Sandbox
+
+[license-image]: http://img.shields.io/badge/license-MIT-blue.svg
+[license]: LICENSE
---
+++ src/environments/set-env.ts
@@ -0,0 +1,67 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 PHAST
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import ErrnoException = NodeJS.ErrnoException;
+
+const setEnv = () => {
+    const fs = require('fs');
+    const writeFile = fs.writeFile;
+// Configure Angular `environment.ts` file path
+    const targetPath = './src/environments/environment.ts';
+// Load node modules
+    const colors = require('colors');
+    const appVersion = require('../../package.json').version;
+    require('dotenv').config({
+        path: 'src/environments/.env'
+    });
+// `environment.ts` file structure
+    const envConfigFile = `export const environment = {
+  npm_package_version: '${appVersion}',
+  client_id: '${process.env.client_id}',
+  cio_dc_url: 'https://recette.phast.fr/resources-server_Atelier/api/FHIR',
+  cio_dc_credential: '${process.env.cio_dc_credential}',
+  tio_url: 'https://recette.phast.fr/resources-server_Atelier/api/FHIR',
+  tio_credential: '${process.env.tio_credential}',
+  display_language: 'fr-FR',
+  cds_hooks_url: 'https://cds-access.phast.fr:8443/cql-cds-hooks',
+  cql_service_url: 'https://cds-access.phast.fr:8443/cql-proxy/r4/fhir',
+  fhir_date_short_format: 'yyyy-MM-dd\\'T\\'HH:mm:00',
+  fhir_date_format: 'yyyy-MM-dd\\'T\\'HH:mm:ss',
+  display_date_format: 'dd/MM/yyyy HH:mm',
+  drug_formulary_resource_type: 'MedicationKnowledge',
+  production: true
+};
+`;
+    console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+    writeFile(targetPath, envConfigFile, (err: ErrnoException | null) => {
+        if (err) {
+            console.error(err);
+            throw err;
+        }
+        else {
+            console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+        }
+    });
+};
+setEnv();
