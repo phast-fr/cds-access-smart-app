20dfe8aa514ecb940dfd99ef072960962ba316d6
David Ouagne <david.ouagne@phast.fr>
2022-02-01 18:19:31 +0100

    fix(ci): add test client_id into fhir.smart.service.ts file

--- src/app/common/fhir/smart/services/fhir.smart.service.ts
+++ src/app/common/fhir/smart/services/fhir.smart.service.ts
@@ -72,7 +72,7 @@ export class FhirSmartService {
       console.error('context is not supported', context);
       return;
     }
-    if (environment.client_id.hasOwnProperty(context)
+    if (environment?.client_id?.hasOwnProperty(context)
         && !environment.client_id[context]) {
       console.error('client_id is not supported for context', context);
       return;
