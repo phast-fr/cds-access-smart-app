f12ddba13bfeb6f312b1b9eda43b5acfee0ddba6
David Ouagne <david.ouagne@phast.fr>
2022-02-02 11:20:25 +0100

    refactor(smart on FHIR): improve class structure

--- src/app/common/fhir/smart/services/fhir.smart.service.ts
+++ src/app/common/fhir/smart/services/fhir.smart.service.ts
@@ -63,102 +63,26 @@ export class FhirSmartService {
   }
 
   public launch(context: string, iss: string, launch: string, redirectUri?: string): void {
-    if (!context) {
-      console.error('context cannot be undefined');
-      return;
+    const params = this.getLaunchParams(context, iss, launch, redirectUri);
+    if (params) {
+      this._fhirClient.smartAuthMetadata(iss, this.getHttpOptions())
+          .subscribe({
+            next: metadata => {
+              window.location.href = metadata.authorizeUrl.href + '?' + params.toString();
+            },
+            error: err => console.error('error', err)
+          });
     }
-    if (context !== 'prescription' && context !== 'formulary'
-        && context !== 'dispense' && context !== 'cql-editor') {
-      console.error('context is not supported', context);
-      return;
-    }
-    if (!environment.client_id) {
-      console.error('client_id is undefined');
-      return;
-    }
-    if (!environment.client_id[context]) {
-      console.error('client_id is not supported for context', context);
-      return;
-    }
-    const clientId = environment.client_id[context];
-    sessionStorage.clear();
-    sessionStorage.setItem('context', context);
-    this.setBaseUrl(iss);
-    sessionStorage.setItem('launch', launch);
-    if (redirectUri) {
-      sessionStorage.setItem('redirect_uri', redirectUri);
-    }
-
-    const state = nanoid(16);
-
-    const options = {
-      headers: new HttpHeaders()
-        .set('Accept', 'application/fhir+json,application/json')
-        .set('Content-type', 'application/fhir+json')
-    } as Options;
-
-    this._fhirClient.smartAuthMetadata(iss, options)
-      .subscribe({
-        next: metadata => {
-          let params = new HttpParams()
-            .set('response_type', 'code')
-            .set('client_id', clientId)
-            .set('launch', launch)
-            .set('scope', environment.scope[context])
-            .set('state', state)
-            .set('aud', iss);
-
-          if (redirectUri) {
-            params = params.set('redirect_uri', redirectUri);
-          }
-
-          window.location.href = metadata.authorizeUrl.href + '?' + params.toString();
-        },
-        error: err => console.error('error', err)
-      });
   }
 
   public retrieveContext(code: string, state: string): void {
-    const context = sessionStorage['context'];
-    if (typeof context !== 'string') {
-      console.error('context is not string type', context);
-      return;
-    }
-    if (context !== 'prescription' && context !== 'formulary'
-        && context !== 'dispense' && context !== 'cql-editor') {
-      console.error('context is not supported', context);
-      return;
-    }
-    if (!environment.client_id) {
-      console.error('client_id is undefined');
-      return;
-    }
-    const clientId = environment.client_id[context];
-    const options = {
-      headers: new HttpHeaders()
-        .set('Accept', 'application/fhir+json,application/json')
-        .set('Content-type', 'application/fhir+json')
-    } as Options;
-
+    const body = this.retrieveContextBody(code, state);
     const baseUrl = this.getBaseUrl();
-    if (baseUrl) {
-      this._fhirClient.smartAuthMetadata(baseUrl, options)
+    if (baseUrl && body) {
+      this._fhirClient.smartAuthMetadata(baseUrl, this.getHttpOptions())
         .subscribe({
           next: metadata => {
-            if (context) {
-              let body = new HttpParams()
-                  .set('grant_type', 'authorization_code')
-                  .set('client_id', clientId)
-                  .set('code', code)
-                  .set('state', state);
-
-              const redirectUri = sessionStorage.getItem('redirect_uri');
-              if (redirectUri) {
-                body = body.set('redirect_uri', redirectUri);
-              }
-
-              this.doPostToken(metadata.tokenUrl.href, body.toString());
-            }
+            this.doPostToken(metadata.tokenUrl.href, body.toString());
           },
           error: err => console.error('error', err)
         });
@@ -170,29 +94,13 @@ export class FhirSmartService {
   }
 
   public refreshContext(): void {
-    const refreshToken = sessionStorage.getItem('refresh_token');
-    if (!refreshToken) { return; }
-    const options = {
-      headers: new HttpHeaders()
-        .set('Accept', 'application/fhir+json,application/json')
-        .set('Content-type', 'application/fhir+json')
-    } as Options;
-
+    const body = this.refreshContextBody();
     const baseUrl = this.getBaseUrl();
-    if (baseUrl) {
-      this._fhirClient.smartAuthMetadata(baseUrl, options)
+    if (baseUrl && body) {
+      this._fhirClient.smartAuthMetadata(baseUrl, this.getHttpOptions())
         .subscribe({
           next: metadata => {
-            const context = sessionStorage.getItem('context');
-            if (context === 'prescription' || context === 'formulary'
-                || context === 'dispense' || context === 'cql-editor') {
-              const body = new HttpParams()
-                .set('grant_type', 'refresh_token')
-                .set('refresh_token', refreshToken)
-                .set('scope', environment.scope[context]);
-
-              this.doPostToken(metadata.tokenUrl.href, body.toString());
-            }
+            this.doPostToken(metadata.tokenUrl.href, body.toString());
           },
           error: err => console.error('error', err)
         });
@@ -204,37 +112,10 @@ export class FhirSmartService {
     if (baseUrl) {
       context.iss = baseUrl;
     }
-    this.setAccessToken(context.access_token);
-    sessionStorage.setItem('expires_in', String(context.expires_in));
-    sessionStorage.setItem('expire_date', String(new Date().getTime() + (1000 * context.expires_in)));
-    sessionStorage.setItem('scope', context.scope);
-    sessionStorage.setItem('token_type', context.token_type);
-    sessionStorage.setItem('id_token', context.id_token);
-    sessionStorage.setItem('patient', context.patient);
-    sessionStorage.setItem('need_patient_banner', String(context.need_patient_banner));
-    if (context.refresh_token) {
-      sessionStorage.setItem('refresh_token', context.refresh_token);
-    }
-    if (context.intent) {
-      sessionStorage.setItem('intent', context.intent);
-    }
-    if (context.service_id) {
-      sessionStorage.setItem('service_id', context.service_id);
-    }
+    this.setContextToSession(context);
 
-    const user = this.getUser<SmartUser>(context.id_token);
-    const state = new StateModel();
-    state.context = context;
-    state.user = user;
-    state.needPatientBanner = context.need_patient_banner;
-    state.intent = context.intent;
-
-    const options = {
-      headers: new HttpHeaders()
-        .set('Accept', 'application/fhir+json,application/json')
-        .set('Content-type', 'application/fhir+json')
-        .set('Authorization', `Bearer ${this.getAccessToken()}`)
-    } as Options;
+    const state = this.getStateModel(context);
+    const options = this.getHttpOptionsWithAccessToken();
 
     if (context.iss) {
       if (context.iss && context.patient) {
@@ -290,59 +171,9 @@ export class FhirSmartService {
       }
     }
     else {
-      const context = {} as SmartContext;
-      const baseUrl = this.getBaseUrl();
-      if (baseUrl) {
-        context.iss = baseUrl;
-      }
-      const accessToken = this.getAccessToken();
-      if (accessToken) {
-        context.access_token = accessToken;
-      }
-      context.expires_in = Number(sessionStorage.getItem('expires_in'));
-      const scope = sessionStorage.getItem('scope');
-      if (scope) {
-        context.scope = scope;
-      }
-      const tokenType = sessionStorage.getItem('token_type');
-      if (tokenType) {
-        context.token_type = tokenType;
-      }
-      const idToken = sessionStorage.getItem('id_token');
-      if (idToken) {
-        context.id_token = idToken;
-      }
-      const patientId = sessionStorage.getItem('patient');
-      if (patientId) {
-        context.patient = patientId;
-      }
-      context.need_patient_banner = sessionStorage.getItem('need_patient_banner') === 'true';
-      const intent = sessionStorage.getItem('intent');
-      if (intent) {
-        context.intent = intent;
-      }
-      const serviceId = sessionStorage.getItem('service_id');
-      if (serviceId) {
-        context.service_id = serviceId;
-      }
-
-      const state = new StateModel();
-      state.context = context;
-      if (context.id_token) {
-        state.user = this.getUser<SmartUser>(context.id_token);
-      }
-      state.needPatientBanner = context.need_patient_banner;
-      if (context.intent) {
-        state.intent = context.intent;
-      }
-
-      const options = {
-        headers: new HttpHeaders()
-          .set('Accept', 'application/fhir+json,application/json')
-          .set('Content-type', 'application/fhir+json')
-          .set('Authorization', `Bearer ${this.getAccessToken()}`)
-      } as Options;
-
+      const context = this.getContextFromSession();
+      const state = this.getStateModel(context);
+      const options = this.getHttpOptionsWithAccessToken();
       if (context.iss) {
         if (context.patient && context.patient !== 'undefined') {
           this._fhirClient.read(context.iss, {
@@ -404,6 +235,49 @@ export class FhirSmartService {
     return Utils.jwtDecode(tokenId) as any;
   }
 
+  private getLaunchParams(context: string, iss: string, launch: string, redirectUri?: string): HttpParams | boolean {
+    if (!context) {
+      console.error('context cannot be undefined');
+      return false;
+    }
+    if (context !== 'prescription' && context !== 'formulary'
+        && context !== 'dispense' && context !== 'cql-editor') {
+      console.error('context is not supported', context);
+      return false;
+    }
+    if (!environment.client_id) {
+      console.error('client_id is undefined');
+      return false;
+    }
+    if (!environment.client_id[context]) {
+      console.error('client_id is not supported for context', context);
+      return false;
+    }
+    const clientId = environment.client_id[context];
+    sessionStorage.clear();
+    sessionStorage.setItem('context', context);
+    this.setBaseUrl(iss);
+    sessionStorage.setItem('launch', launch);
+    if (redirectUri) {
+      sessionStorage.setItem('redirect_uri', redirectUri);
+    }
+
+    const state = nanoid(16);
+
+    let params = new HttpParams()
+        .set('response_type', 'code')
+        .set('client_id', clientId)
+        .set('launch', launch)
+        .set('scope', environment.scope[context])
+        .set('state', state)
+        .set('aud', iss);
+
+    if (redirectUri) {
+      params = params.set('redirect_uri', redirectUri);
+    }
+    return params;
+  }
+
   private setBaseUrl(value: string): void {
     sessionStorage.setItem('iss', value);
     this._baseUrl$.next(value);
@@ -451,5 +325,141 @@ export class FhirSmartService {
       },
       error: err => console.error('error', err)
     });
+  }
+
+  private retrieveContextBody(code: string, state: string): HttpParams | boolean {
+    const context = sessionStorage['context'];
+    if (typeof context !== 'string') {
+      console.error('context is not string type', context);
+      return false;
+    }
+    if (context !== 'prescription' && context !== 'formulary'
+        && context !== 'dispense' && context !== 'cql-editor') {
+      console.error('context is not supported', context);
+      return false;
+    }
+    if (!environment.client_id) {
+      console.error('client_id is undefined');
+      return false;
+    }
+    const clientId = environment.client_id[context];
+    let body = new HttpParams()
+        .set('grant_type', 'authorization_code')
+        .set('client_id', clientId)
+        .set('code', code)
+        .set('state', state);
+
+    const redirectUri = sessionStorage.getItem('redirect_uri');
+    if (redirectUri) {
+      body = body.set('redirect_uri', redirectUri);
+    }
+    return body;
+  }
+
+  private refreshContextBody(): HttpParams | boolean {
+    const refreshToken = sessionStorage.getItem('refresh_token');
+    if (!refreshToken) {
+      console.error('refresh token is absent to session storage');
+      return false;
+    }
+    const context = sessionStorage.getItem('context');
+    if (context !== 'prescription' && context !== 'formulary'
+        && context !== 'dispense' && context !== 'cql-editor') {
+      console.error('context is not supported', context);
+      return false;
+    }
+    return new HttpParams()
+        .set('grant_type', 'refresh_token')
+        .set('refresh_token', refreshToken)
+        .set('scope', environment.scope[context]);
+  }
+
+  private setContextToSession(context: SmartContext): void {
+    this.setAccessToken(context.access_token);
+    sessionStorage.setItem('expires_in', String(context.expires_in));
+    sessionStorage.setItem('expire_date', String(new Date().getTime() + (1000 * context.expires_in)));
+    sessionStorage.setItem('scope', context.scope);
+    sessionStorage.setItem('token_type', context.token_type);
+    sessionStorage.setItem('id_token', context.id_token);
+    sessionStorage.setItem('patient', context.patient);
+    sessionStorage.setItem('need_patient_banner', String(context.need_patient_banner));
+    if (context.refresh_token) {
+      sessionStorage.setItem('refresh_token', context.refresh_token);
+    }
+    if (context.intent) {
+      sessionStorage.setItem('intent', context.intent);
+    }
+    if (context.service_id) {
+      sessionStorage.setItem('service_id', context.service_id);
+    }
+  }
+
+  private getContextFromSession(): SmartContext {
+    const context = {} as SmartContext;
+    const baseUrl = this.getBaseUrl();
+    if (baseUrl) {
+      context.iss = baseUrl;
+    }
+    const accessToken = this.getAccessToken();
+    if (accessToken) {
+      context.access_token = accessToken;
+    }
+    context.expires_in = Number(sessionStorage.getItem('expires_in'));
+    const scope = sessionStorage.getItem('scope');
+    if (scope) {
+      context.scope = scope;
+    }
+    const tokenType = sessionStorage.getItem('token_type');
+    if (tokenType) {
+      context.token_type = tokenType;
+    }
+    const idToken = sessionStorage.getItem('id_token');
+    if (idToken) {
+      context.id_token = idToken;
+    }
+    const patientId = sessionStorage.getItem('patient');
+    if (patientId) {
+      context.patient = patientId;
+    }
+    context.need_patient_banner = sessionStorage.getItem('need_patient_banner') === 'true';
+    const intent = sessionStorage.getItem('intent');
+    if (intent) {
+      context.intent = intent;
+    }
+    const serviceId = sessionStorage.getItem('service_id');
+    if (serviceId) {
+      context.service_id = serviceId;
+    }
+    return context;
+  }
+
+  private getStateModel(context: SmartContext): StateModel {
+    const state = new StateModel();
+    state.context = context;
+    if (context.id_token) {
+      state.user = this.getUser<SmartUser>(context.id_token);
+    }
+    state.needPatientBanner = context.need_patient_banner;
+    if (context.intent) {
+      state.intent = context.intent;
+    }
+    return state;
+  }
+
+  private getHttpOptions(): Options {
+    return  {
+      headers: new HttpHeaders()
+          .set('Accept', 'application/fhir+json,application/json')
+          .set('Content-type', 'application/fhir+json')
+    } as Options;
+  }
+
+  private getHttpOptionsWithAccessToken(): Options {
+    return  {
+      headers: new HttpHeaders()
+          .set('Accept', 'application/fhir+json,application/json')
+          .set('Content-type', 'application/fhir+json')
+          .set('Authorization', `Bearer ${this.getAccessToken()}`)
+    } as Options;
   }
 }
